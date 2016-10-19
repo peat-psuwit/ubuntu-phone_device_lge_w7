d45d35ed6a636ffdd40bd671bafc3c799e5962b0
Ratchanan Srirattanamet <peathot@hotmail.com>
2016-10-19 23:55:44 +0700

    Revert "Copy proprietary file but not put in system directly."
    I think this should be pre-patched in the vendor tree.
    
    This reverts commit 21c0fb3782afb61910126a253e5b40a64aa1e631.

--- extract-files.sh
+++ extract-files.sh
@@ -25,7 +25,6 @@ BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
 for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$ | grep -v ^-`; do
-    FILE=`echo $FILE|sed -e 's/^%//'`
     DIR=`dirname $FILE`
     if [ ! -d $BASE/$DIR ]; then
         mkdir -p $BASE/$DIR
